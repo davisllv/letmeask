825eb9cfaf7ad1d8ac2aaeebbc214748d75199dd
davisllv <davisllvsnt57@gmail.com>
2021-12-31 06:53:15 -0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
-import { AuthContextProvider } from "./context/Authcontext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 
-import { AuthContext } from "../context/Authcontext";
+import { AuthContext } from "../context/AuthContext";
 
 export function useAuth() {
     const value = useContext(AuthContext)
